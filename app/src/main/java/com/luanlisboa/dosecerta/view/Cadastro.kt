f34409468d14646a00cabeed3c0368687b7c6146
lisboaluan <167384723+lisboaluan@users.noreply.github.com>
2024-08-03 18:44:56 -0300

    Adicionado o front da tela de login (ainda incompleto)

---
+++ app/src/main/java/com/luanlisboa/dosecerta/view/Cadastro.kt
@@ -0,0 +1,12 @@
+package com.luanlisboa.dosecerta.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.luanlisboa.dosecerta.R
+
+class Cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
---
+++ app/src/main/java/com/luanlisboa/dosecerta/view/Home.kt
@@ -0,0 +1,13 @@
+package com.luanlisboa.dosecerta.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.luanlisboa.dosecerta.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
