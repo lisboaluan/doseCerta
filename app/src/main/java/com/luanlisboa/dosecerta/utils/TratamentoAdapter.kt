59fdeb5b153773383bc5ceb79d2d687317abf426
Alexandre Stiehl <115179448+xandrestiehl1@users.noreply.github.com>
2024-09-22 21:09:26 -0300

    Mostra todas anotações do usuário (#20)

--- app/src/main/java/com/luanlisboa/dosecerta/utils/TratamentoAdapter.kt
+++ app/src/main/java/com/luanlisboa/dosecerta/utils/TratamentoAdapter.kt
@@ -12,9 +12,9 @@ class TratamentoAdapter(private val tratamentos: List<Medicamento>) :
     RecyclerView.Adapter<TratamentoAdapter.TratamentoViewHolder>() {
 
     class TratamentoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nomeMedicamento: TextView = view.findViewById(R.id.tvNomeMedicamento)
+        val nomeMedicamento: TextView = view.findViewById(R.id.tvTituloAnotacao)
         val dosagem: TextView = view.findViewById(R.id.tvDosagem)
-        val horario: TextView = view.findViewById(R.id.tvHorario)
+        val horario: TextView = view.findViewById(R.id.tvMensagemAnotacao)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TratamentoViewHolder {
